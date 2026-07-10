ff3831d9ff6ec4e17470caa43f4bf21b49ca54d1
Erkin546 <gulyamovaziz77@gmail.com>
2026-07-10 18:58:45 +0500

    Даня лучший

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,11 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'admin', 
-          dest: './'    
-        }
-      ]
-    })
-  ],
+  
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        room1: resolve(__dirname, 'src/pages/room/room1.html'),
-        room2: resolve(__dirname, 'src/pages/room/room2.html'),
-        room3: resolve(__dirname, 'src/pages/room/room3.html'),
-        room4: resolve(__dirname, 'src/pages/room/room4.html'),
-        room5: resolve(__dirname, 'src/pages/room/room5.html'),
-        room6: resolve(__dirname, 'src/pages/room/room6.html'),
+        main: 'index.html' 
       }
     }
   }
